96f19bfdc9cd826db765f1112e8406895ba34ddc
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-01-31 15:28:54 -0500

    Edit container to fluid on about page

--- page-about.php
+++ page-about.php
@@ -3,9 +3,9 @@
 <?php get_header(); ?>
 
 <div class="main-content">
-  <div class="container">
+  <div class="container-fluid">
     <div class="row">
-      <div class="col-md-9">
+      <div class="col-md-12">
         <?php
           if(have_posts()){
             while(have_posts()){
@@ -14,10 +14,6 @@
             }
           }
         ?>
-      </div>
-      <div class="col-md-3">
-        <?php get_sidebar(); ?>
-
       </div>
     </div>
   </div>
