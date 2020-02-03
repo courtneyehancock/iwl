0895c6a0a694e2fb2df278658843f6129d25791c
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-02-03 16:12:27 -0500

    Edit default page

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 
 <div class="container">
     <div class="row">
-      <div class="col-md-9">
+      <div class="col-md-12">
         <?php
           if(have_posts()){
             while(have_posts()){
@@ -11,10 +11,6 @@
             }
           }
         ?>
-      </div>
-      <div class="col-md-3">
-        <?php get_sidebar(); ?>
-
       </div>
     </div>
 </div>
