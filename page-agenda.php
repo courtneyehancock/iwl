8e8554d0cea160d67dae02c049a5f03ff9171785
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-02-11 16:21:42 -0500

    Edit php template for agenda

--- page-agenda.php
+++ page-agenda.php
@@ -5,7 +5,19 @@
 <div class="main-content">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-md-9">
+      <div class="col-md-12">
+        <?php
+          if(have_posts()){
+            while(have_posts()){
+              the_post();?>
+              <?php the_content();
+            }
+          }
+        ?>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
         <?php
           if(have_posts()){
             while(have_posts()){
