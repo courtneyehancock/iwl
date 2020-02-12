fd59b7b974044d981362509621743428e559c14d
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-02-12 14:11:01 -0500

    Edit templates for location and speaker pages

--- page-location.php
+++ page-location.php
@@ -5,7 +5,7 @@
 <div class="main-content">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-md-9">
+      <div class="col-md-12">
         <?php
           if(have_posts()){
             while(have_posts()){
@@ -14,10 +14,6 @@
             }
           }
         ?>
-      </div>
-      <div class="col-md-3">
-        <?php get_sidebar('right-sidebar'); ?>
-
       </div>
     </div>
   </div>
