0763f2b3b1e592c605b4e9d788ff29d1576b2915
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-02-12 09:38:32 -0500

    Edit agenda template

--- page-agenda.php
+++ page-agenda.php
@@ -17,24 +17,6 @@
       </div>
     </div>
   </div>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-8">
-        <?php
-          if(have_posts()){
-            while(have_posts()){
-              the_post();?>
-              <?php the_content();
-            }
-          }
-        ?>
-      </div>
-      <div class="col-md-3">
-        <?php get_sidebar('right-sidebar'); ?>
-
-      </div>
-    </div>
-  </div>
 </div>
 
 <?php get_footer(); ?>
